b8b2ec72a9f6a863a3886bba266b658a3f02c8ea
Laurie Anderson <laurieanderson92@hotmail.co.uk>
2024-03-06 11:26:44 +0000

    build: Set up Structure of game function

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+function runGame(){
+
+}
+
+function chechAnswer(params) {
+    
+}
+
+function calculateCorrectAnswer(params) {
+    
+}
+
+function incrementScore(params) {
+    
+}
+
+function incrementWrongAwnser(params) {
+    
+}
+
+function displayAdditionQuestion(params) {
+    
+}
+
+function displaySubtractQuestion(params) {
+    
+}
+
+function displayMultiplyQuestion(params) {
+    
+}
+
+// space for division question
